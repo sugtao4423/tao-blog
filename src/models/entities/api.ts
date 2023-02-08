b124f53a337dde35b8e83e0d86ddea507ac870e0
sugtao4423 <sugtao4423@gmail.com>
2023-02-09 07:31:56 +0900

    Create GET method api types

---
+++ src/models/entities/api.ts
@@ -0,0 +1,39 @@
+export type GetUser = {
+  readonly id: number
+  name: string
+  url: string
+  createdAt: number
+}
+
+export type GetTag = {
+  readonly id: number
+  name: string
+  createdAt: number
+}
+
+export type GetComment = {
+  readonly id: number
+  parentId: number | null
+  postId: number
+  author: {
+    id: number
+    name: string
+    url: string | null
+  } | null
+  content: string
+  createdAt: number
+}
+
+export type GetPost = {
+  readonly id: number
+  title: string
+  content: string
+  abstract: string
+  thumbnailUrl: string | null
+  author: GetUser
+  tags: GetTag[]
+  status: 'publish' | 'draft' | 'hidden'
+  commentable: boolean
+  createdAt: number
+  updatedAt: number
+}
