23215783d202c73e1898272eb50b94edaf4c8fce
sugtao4423 <sugtao4423@gmail.com>
2023-02-12 08:06:46 +0900

    Fix lint `any`

--- src/@types/kysely.d.ts
+++ src/@types/kysely.d.ts
@@ -1,6 +1,7 @@
 import 'kysely'
 
 declare module 'kysely' {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   export declare type LeftJoinedDB<DB, A extends keyof any, R> = {
     [C in keyof DB | A]: C extends A
       ? Nullable<R>
